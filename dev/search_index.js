e6d80431afec17334aba1fa56b84868dec8c5c02
Documenter.jl <documenter@juliadocs.github.io>
2024-07-11 16:19:45 +0000

    build based on 89aa856

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#Kepler.jl","page":"Introduction","title":"Kepler.jl","text":"","category":"section"},{"location":"index.html","page":"Introduction","title":"Introduction","text":"Kepler.jl is part of the control-toolbox ecosystem.","category":"page"},{"location":"index.html","page":"Introduction","title":"Introduction","text":"note: Install\nTo install a package from the control-toolbox ecosystem,  please visit the installation page.","category":"page"}]
+}
