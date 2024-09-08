89e5689da67233ab6480e6c459b1aa29c76d942f
ocots <66357348+ocots@users.noreply.github.com>
2024-09-08 00:05:35 +0000

    :robot: Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,17 @@
 using Documenter
 
 makedocs(;
-    warnonly = [:cross_references, :autodocs_block],
-    sitename = "kepler",
-    format = Documenter.HTML(
-        prettyurls = false,
-        assets = [
+    warnonly=[:cross_references, :autodocs_block],
+    sitename="kepler",
+    format=Documenter.HTML(;
+        prettyurls=false,
+        assets=[
             asset("https://control-toolbox.org/assets/css/documentation.css"),
             asset("https://control-toolbox.org/assets/js/documentation.js"),
         ],
     ),
-    pages = ["Orbit transfer" => "index.md"],
-    checkdocs = :none,
+    pages=["Orbit transfer" => "index.md"],
+    checkdocs=:none,
 )
 
-deploydocs(repo = "github.com/control-toolbox/Kepler.jl.git", devbranch = "main")
+deploydocs(; repo="github.com/control-toolbox/Kepler.jl.git", devbranch="main")
