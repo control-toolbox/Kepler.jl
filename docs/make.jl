a996c554b309b0aef0f5b1c51a9e39f44666194e
Olivier Cots <66357348+ocots@users.noreply.github.com>
2024-09-10 21:08:53 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,12 @@
 using Documenter
 
+repo_url = "github.com/control-toolbox/Kepler.jl"
+
 makedocs(;
     warnonly=[:cross_references, :autodocs_block],
     sitename="kepler",
     format=Documenter.HTML(;
+        repolink = "https://"*repo_url,
         prettyurls=false,
         assets=[
             asset("https://control-toolbox.org/assets/css/documentation.css"),
@@ -14,4 +17,6 @@ makedocs(;
     checkdocs=:none,
 )
 
-deploydocs(; repo="github.com/control-toolbox/Kepler.jl.git", devbranch="main")
+deploydocs(;
+    repo=repo_url*".git", devbranch="main"
+)
